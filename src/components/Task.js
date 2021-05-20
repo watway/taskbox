1bbee577e6dcfb9ec25d1c75188135803c5d1e33
watway <craig@watway.net>
2021-05-20 17:26:01 +1000

    check task background to red

--- src/components/Task.js
+++ src/components/Task.js
@@ -11,17 +11,22 @@ export default function Task({ task: { id, title, state }, onArchiveTask, onPinT
           disabled={true}
           name="checked"
         />
-        <span className="checkbox-custom" onClick={() => onArchiveTask(id)}/>
+        <span className="checkbox-custom" onClick={() => onArchiveTask(id)} />
       </label>
       <div className="title">
-        <input type="text" value={title} readOnly={true} placeholder="Input title"/>
+        <input type="text"
+          value={title}
+          readOnly={true}
+          placeholder="Input title"
+          style={{ background: 'red' }}
+        />
       </div>
 
       <div className="actions" onClick={event => event.stopPropagation()}>
         {state !== 'TASK_ARCHIVED' && (
           // eslint-disable-next-line jsx-a11y/anchor-is-valid
           <a onClick={() => onPinTask(id)}>
-            <span className={`icon-star`}/>
+            <span className={`icon-star`} />
           </a>
         )}
       </div>
